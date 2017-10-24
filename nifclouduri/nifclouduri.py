b336033ab722e336880d2a07946d0534bc4c81e8
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-10-25 00:05:24 +0900

    Signature生成修正

--- nifclouduri/nifclouduri.py
+++ nifclouduri/nifclouduri.py
@@ -2,15 +2,10 @@
 # condig: utf-8
 
 import hashlib
-import urllib
 import hmac
 import base64
 
 class Signature:
     @staticmethod
     def v0(key, data):
-        return urllib.quote_plus(
-            base64.b64encode(
-                hmac.new(key.encode('utf-8'), data.encode('utf-8'), hashlib.sha256).digest()
-            )
-        )
+        return base64.b64encode(hmac.new(key.encode('utf-8'), data.encode('utf-8'), hashlib.sha1).digest())
