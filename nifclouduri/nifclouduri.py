3b36d751fae02d06522aba1ad08dc6fa0b3795c2
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-10-26 00:25:03 +0900

    requestメソッド追加

--- nifclouduri/nifclouduri.py
+++ nifclouduri/nifclouduri.py
@@ -20,3 +20,18 @@ class NifCloud:
         self.access_key = access_key
         self.secret_key = secret_key
 
+    def request(self, action, query={}):
+        self.set_query(action, query)
+
+        return requests.get(self.endpoint, params=query)
+
+    def set_query(self, action, query={}):
+        timestamp = dt.now().strftime('%Y-%m-%dT%XZ')
+        query.update({
+            'Action': action,
+            'Signature': Signature.v0(self.secret_key, f"{action}{timestamp}"),
+            'SignatureVersion': 0,
+            'AccessKeyId': self.access_key,
+            'Timestamp': timestamp,
+        })
+
